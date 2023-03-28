ab881ac845d423bb7cd78670d968c55a47c9ad0a
Nicolas <alternative.rvb@gmail.com>
2023-03-28 15:42:27 +0200

    MAJ

--- example/js/main.js
+++ example/js/main.js
@@ -23,6 +23,84 @@ includeElmt("./components/footer.html").then((data) => {
 
 regionFilter();
 
+async function fetchKitsuData() {
+  const apiUrl = `https://kitsu.io/api/edge/manga?page[limit]=18`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  console.log(data);
+  displayKitsuData(data.data);
+}
+
+function displayKitsuData(results) {
+  const resultsContainer = document.getElementById("kitsu-results");
+  resultsContainer.innerHTML = ""; // Clear the current content
+
+  results.forEach(result => {
+    const manga = result.attributes;
+    const coverUrl = manga.posterImage.medium || "https://placehold.co/195x277/CC66FF/FFFFFF/?text=NO+COVER&font=oxygen";
+
+    const article = document.createElement("article");
+    article.classList.add("art-book", "d-flex", "g-1", "p-1", "rounded");
+
+    const leftDiv = document.createElement("div");
+    leftDiv.classList.add("art-book__left");
+
+    const img = document.createElement("img");
+    img.src = coverUrl;
+    img.alt = manga.titles.en || manga.titles.en_jp || "Titre du livre";
+    img.loading = "lazy";
+    leftDiv.appendChild(img);
+
+    const rightDiv = document.createElement("div");
+    rightDiv.classList.add("art-book__right", "d-flex", "fd-column");
+
+    const h3 = document.createElement("h3");
+    h3.textContent = manga.titles.en || manga.titles.en_jp || "Titre du livre";
+    rightDiv.appendChild(h3);
+
+    const pSynopsis = document.createElement("p");
+    const synopsis = manga.synopsis || "";
+    pSynopsis.textContent = synopsis.length > 150 ? synopsis.substring(0, 150) + "..." : synopsis;
+    rightDiv.appendChild(pSynopsis);
+
+    const pMeta = document.createElement("p");
+    pMeta.classList.add("meta");
+    
+    const authorSpan = document.createElement("span");
+    authorSpan.classList.add("meta__author");
+    const authorIcon = document.createElement("i");
+    // date
+    authorIcon.classList.add("fa-solid", "fa-calendar");
+    const authorText = document.createTextNode(` Date: ${manga.startDate || 'Inconnu'}`);
+    authorSpan.appendChild(authorIcon);
+    authorSpan.appendChild(authorText);
+    
+    const categorySpan = document.createElement("span");
+    categorySpan.classList.add("meta__category");
+    const categoryIcon = document.createElement("i");
+    categoryIcon.classList.add("fa-solid", "fa-tag");
+    const categoryText = document.createTextNode(` Type: ${manga.mangaType || 'Inconnue'}`);
+    categorySpan.appendChild(categoryIcon);
+    categorySpan.appendChild(categoryText);
+    
+    pMeta.appendChild(authorSpan);
+    pMeta.appendChild(document.createTextNode(' | '));
+    pMeta.appendChild(categorySpan);
+    rightDiv.appendChild(pMeta);
+
+    article.appendChild(leftDiv);
+    article.appendChild(rightDiv);
+    resultsContainer.appendChild(article);
+  });
+}
+
+
+
+fetchKitsuData();
+
+
+
+
 //----------------------------------------------------------------------
 
 // class MyHeader extends HTMLElement {
